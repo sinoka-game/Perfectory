808bd968ed1d71db99c55b2739d0da4f601286f1
sinokadev <sinoka@sinoka.dev>
2025-12-14 07:10:33 +0900

    Add conveyor.h and conveyor.c

---
+++ include/conveyor.h
@@ -0,0 +1,13 @@
+#ifndef CONVEYOR_H
+#define CONVEYOR_H
+
+typedef struct conveyor{
+    char texture_path[255];
+};
+
+int PER_conveyor_connect(int up, int down, int left, int right);
+// input: 특정 컨베이어 블록을 기준으로 1칸 이내에 인접한 다른 컨베이어 블록을 나타냄. 컨베이어 벨트가 있으면 1, 없으면 0
+// return: 정 가운데의 컨베이어 벨트의 방향을 리턴해야함 
+// │ : 0, ─ : 1, ┌ : 2, ┐ : 3, └ : 4, ┘ : 5
+
+#endif
---
+++ src/block/conveyor.c
@@ -0,0 +1,6 @@
+#include "conveyor.h"
+
+int PER_conveyor_connect(int up, int down, int left, int right) {
+    // 구현 필요
+    return 0;
+}
